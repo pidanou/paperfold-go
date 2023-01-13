fcae3050374d9a7569417d30d3482dfaf2e16ead
pidanou <pidanoueang@gmail.com>
2023-01-14 00:54:01 +0100

    add packages

---
+++ go.mod
@@ -0,0 +1,5 @@
+module practice/paperfold-go
+
+go 1.18
+
+require github.com/pidanou/go-matrix-lib v0.0.4
--- main.go
+++ main.go
@@ -1,2 +1,42 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/pidanou/go-matrix-lib"
+)
+
+func main() {
+
+	// createPaper()
+
+	test := matrix.RotateMatrixClockwise(createPaper())
+
+	fmt.Println(test)
+
+}
+
+func createPaper() [][]string {
+
+	paper := make([][]string, 0)
+	var row = 0
+	var col = 0
+
+	fmt.Println("Input rows then cols lengths:")
+	fmt.Scan(&row, &col)
+
+	for i := 0; i < row; i++ {
+		tmp := make([]string, 0)
+		for j := 0; j < col; j++ {
+			fmt.Printf("Input %dx%v: ", i+1, j+1)
+			var input string
+			fmt.Scan(&input)
+			tmp = append(tmp, input)
+		}
+		paper = append(paper, tmp)
+	}
+
+	fmt.Println(paper)
+
+	return paper
+}
